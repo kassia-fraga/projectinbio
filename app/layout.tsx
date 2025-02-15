bf8191284fcb5ce35fa9e38d2b9951204fedad8f
Kassia Fraga <kassiafraga7@gmail.com>
2025-02-15 20:40:16 -0300

    Add components for landing page: TotalVisits, UserCard, Hero, and UI elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
-import "./globals.css";
+import './globals.css';
+
+import { Red_Hat_Display } from 'next/font/google';
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export default function RootLayout({
   children,
@@ -6,9 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`antialiased`}
+        className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}
       >
         {children}
       </body>
