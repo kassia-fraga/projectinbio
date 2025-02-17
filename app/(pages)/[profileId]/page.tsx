9960d7db4b6ac48b285a3194a3f42ace50fc47d4
Kassia Fraga <kassiafraga7@gmail.com>
2025-02-16 23:25:18 -0300

    Enhance profile page functionality; implement trial subscription check and update user session handling

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -1,16 +1,16 @@
-import { increaseProfileVisits } from '@/app/actions/increase-profile-visits';
-import ProjectCard from '@/app/components/commons/project-card';
-import TotalVisits from '@/app/components/commons/total-visits';
-import UserCard from '@/app/components/commons/user-card/user-card';
-import { auth } from '@/app/lib/auth';
-import { getDownloadURLFromPath } from '@/app/lib/firebase';
+import { increaseProfileVisits } from "@/app/actions/increase-profile-visits";
+import ProjectCard from "@/app/components/commons/project-card";
+import TotalVisits from "@/app/components/commons/total-visits";
+import UserCard from "@/app/components/commons/user-card/user-card";
+import { auth } from "@/app/lib/auth";
+import { getDownloadURLFromPath } from "@/app/lib/firebase";
 import {
   getProfileData,
   getProfileProjects,
-} from '@/app/server/get-profile-data';
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
-import NewProject from './new-project';
+} from "@/app/server/get-profile-data";
+import Link from "next/link";
+import { notFound, redirect } from "next/navigation";
+import NewProject from "./new-project";
 
 export default async function ProfilePage({
   params,
@@ -33,18 +33,23 @@ export default async function ProfilePage({
     await increaseProfileVisits(profileId);
   }
 
-  // Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
+  if (isOwner && !session?.user.isSubscribed && !session?.user.isTrial) {
+    redirect(`/${profileId}/upgrade`);
+  }
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
-      <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
-        <span>Você está usando a versão trial.</span>
-        <Link href={`/${profileId}/upgrade`}>
-          <button className="text-accent-green font-bold">
-            Faça o upgrade agora!
-          </button>
-        </Link>
-      </div>
+      {session?.user.isTrial && !session.user.isSubscribed && (
+        <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
+          <span>Você está usando a versão trial.</span>
+          <Link href={`/${profileId}/upgrade`}>
+            <button className="text-accent-green font-bold">
+              Faça o upgrade agora!
+            </button>
+          </Link>
+        </div>
+      )}
+
       <div className="w-1/2 flex justify-center h-min">
         <UserCard profileData={profileData} isOwner={isOwner} />
       </div>
@@ -54,7 +59,7 @@ export default async function ProfilePage({
             key={project.id}
             project={project}
             isOwner={isOwner}
-            img={(await getDownloadURLFromPath(project.imagePath)) || ''}
+            img={(await getDownloadURLFromPath(project.imagePath)) || ""}
           />
         ))}
         {isOwner && <NewProject profileId={profileId} />}
